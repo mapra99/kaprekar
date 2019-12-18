7814f1d971e0f38f9e5d3656a6da6c00c6582979
Miguel Prada <mapra99@gmail.com>
2019-12-18 16:10:20 -0500

    fixed linter errors

--- app/models/seed.rb
+++ app/models/seed.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class Seed < ApplicationRecord
   has_many :kapregard_processes
 
-  validates :value, numericality: {only_integer: true, greater_than_or_equal_to: 10},
-   presence: true, uniqueness: true
+  validates :value, numericality: { only_integer: true, greater_than_or_equal_to: 10 },
+                    presence: true, uniqueness: true
 end
