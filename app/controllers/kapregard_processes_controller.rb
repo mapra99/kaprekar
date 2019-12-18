6094402ae0cd554cc4c83d017d1c96b3c6b16e69
Miguel Prada <mapra99@gmail.com>
2019-12-18 17:08:12 -0500

    fixed linter errors

--- app/controllers/kapregard_processes_controller.rb
+++ app/controllers/kapregard_processes_controller.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class KapregardProcessesController < ApplicationController
   def index
     @series = Seed.all.map do |seed|
-      {name: seed.value, data: seed.kaprekard_processes.pluck(:iteration, :value)}
+      { name: seed.value, data: seed.kaprekard_processes.pluck(:iteration, :value) }
     end
   end
 end
