c366e250c9fe2f35dc35be7cda18210e5f4c839b
Miguel Prada <mapra99@gmail.com>
2019-12-18 16:30:02 -0500

    added logic of kaprekar

--- app/models/kaprekard_process.rb
+++ app/models/kaprekard_process.rb
@@ -5,4 +5,18 @@ class KaprekardProcess < ApplicationRecord
 
   validates :iteration, presence: true
   validates :value, presence: true
+
+  def self.calculate(num)
+    highest(num) - lowest(num)
+  end
+
+  private
+
+  def self.highest(num)
+    num.digits.sort.reverse.join.to_i
+  end
+
+  def self.lowest(num)
+    num.digits.sort.join.to_i
+  end
 end
--- app/models/seed.rb
+++ app/models/seed.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
 class Seed < ApplicationRecord
-  has_many :kapregard_processes
+  has_many :kaprekard_processes
 
   validates :value, numericality: { only_integer: true, greater_than_or_equal_to: 10 },
                     presence: true, uniqueness: true
+
+  def kaprekard
+    num = kaprekard_processes.create(iteration: 0, value: value)
+    20.times do |i|
+      num = kaprekard_processes.create(iteration: i+1, value: KaprekardProcess.calculate(num.value))
+    end
+  end
 end
