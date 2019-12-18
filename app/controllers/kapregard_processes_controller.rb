8eab96a9290c6dc8d6c9166998546564af96f2d5
Miguel Prada <mapra99@gmail.com>
2019-12-18 16:50:16 -0500

    added gem and view for all seeds visualization

---
+++ app/controllers/kapregard_processes_controller.rb
@@ -0,0 +1,7 @@
+class KapregardProcessesController < ApplicationController
+  def index
+    @series = Seed.all.map do |seed|
+      {name: seed.value, data: seed.kaprekard_processes.pluck(:iteration, :value)}
+    end
+  end
+end
---
+++ app/helpers/kapregard_processes_helper.rb
@@ -0,0 +1,2 @@
+module KapregardProcessesHelper
+end
